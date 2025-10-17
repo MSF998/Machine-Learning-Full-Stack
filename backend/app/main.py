b8414d3ae188c15b20dad7f034616382313fd542
msf998 <mohdsahnoon@gmail.com>
2025-10-17 23:34:38 +0530

    fixed path for render file health check

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/health-check")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/")
+def health_check():
+    return {"status": "ok"}
